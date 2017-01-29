970b239614c74f8246545f24a0d258e16aae0290
rupengkun <522523828@qq.com>
2017-01-30 03:38:42 +0800

    1.04 launch a message from each button

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.myappportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,26 +14,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void actionSpotifyStreamer(View view){
-
+        Toast.makeText(this,"This button will launch my Media Streamer app",Toast.LENGTH_SHORT).show();
     }
 
     public void actionScoresApp(View view){
-
+        Toast.makeText(this,"This button will launch my Score app",Toast.LENGTH_SHORT).show();
     }
 
     public void actionLibaryApp(View view){
-
+        Toast.makeText(this,"This button will launch my Library app",Toast.LENGTH_SHORT).show();
     }
 
     public void actionBuildItBigger(View view){
-
+        Toast.makeText(this,"This button will launch my Build It Bigger app",Toast.LENGTH_SHORT).show();
     }
 
     public void actionXyzReader(View view){
-       
+        Toast.makeText(this,"This button will launch my Reader app",Toast.LENGTH_SHORT).show();
     }
 
     public void actionMyOwnApp(View view){
-
+        Toast.makeText(this,"This button will launch my own app",Toast.LENGTH_SHORT).show();
     }
 }
