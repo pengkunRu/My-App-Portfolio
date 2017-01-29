4dd23e15af1a707670c4a3789df7c2855ea9a5f1
rupengkun <522523828@qq.com>
2017-01-30 04:00:31 +0800

    1.05 Add a launch icon

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
